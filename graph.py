4b2ee8bd8c9c76aceedc278b41b0b0ff3bddd1b8
Gokul PM <mail.gokulpm@gmail.com>
2026-08-16 08:44:47 +0530

    feat: graph

---
+++ graph.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from config import config
+
+class State(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+gemini = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash-lite",
+    api_key=config.gemini
+)
+
+async def llm(state: State):
+    response = await gemini.ainvoke(state["messages"])
+    return {"messages": [response]}
+
+builder = StateGraph(State)
+
+builder.add_node("llm", llm)
+
+builder.add_edge(START, "llm")
+builder.add_edge("llm", END)
+
+graph = builder.compile()
--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import uvicorn
 from config import config
 from fastapi import FastAPI, Response
+from langchain_core.messages import HumanMessage
+from pydantic import BaseModel
+from graph import graph
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return Response()
+
+class ChatRequest(BaseModel):
+    message: str
+    thread_id: str
+
+
+class ChatResponse(BaseModel):
+    message: str
+
+@app.post("/chat", response_model=ChatResponse)
+async def chat(request: ChatRequest):
+    result = await graph.ainvoke(
+        {"messages": [HumanMessage(content=request.message)]},
+        config={"configurable": {"thread_id": request.thread_id}},
+    )
+    message = result["messages"][-1]
+    content = message.content[0]
+    text = content['text']
+    return ChatResponse(message=text)
+
 
 def main():
     if config.reload:
